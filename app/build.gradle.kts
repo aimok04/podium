0fe5b6dd4ceb3ba4badcd850337b92086d43941b
aimok04 <aimok04@mailbox.org>
2026-03-05 22:51:54 +0100

    release: 1.0.0-alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "app.podiumpodcasts.podium"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1000001
-        versionName = "1.0.0-alpha01"
+        versionCode = 1000002
+        versionName = "1.0.0-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
---
+++ fastlane/metadata/changelog.md
@@ -0,0 +1,5 @@
+Hey 👋
+
+This release fixes crashes in some sub routes:
+
+- fix(ui): removed non-null assertions from lazy list keys
