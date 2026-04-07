a7269c6ab22e55c1bd84ec95d1a350af75e98b05
aimok04 <aimok04@mailbox.org>
2026-04-07 16:43:19 +0200

    fix(utils/XML): ignore unknown children (resolved #32)

--- app/src/main/java/app/podiumpodcasts/podium/utils/XML.kt
+++ app/src/main/java/app/podiumpodcasts/podium/utils/XML.kt
@@ -1,8 +1,16 @@
 package app.podiumpodcasts.podium.utils
 
+import nl.adaptivity.xmlutil.ExperimentalXmlUtilApi
+import nl.adaptivity.xmlutil.serialization.DefaultXmlSerializationPolicy
 import nl.adaptivity.xmlutil.serialization.XML
+import nl.adaptivity.xmlutil.serialization.XmlConfig.Companion.IGNORING_UNKNOWN_CHILD_HANDLER
 
+@OptIn(ExperimentalXmlUtilApi::class)
 val xml = XML {
     autoPolymorphic = true
     repairNamespaces = true
+
+    policy = DefaultXmlSerializationPolicy {
+        unknownChildHandler = IGNORING_UNKNOWN_CHILD_HANDLER
+    }
 }
