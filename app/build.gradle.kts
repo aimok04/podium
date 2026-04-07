9030e7dc031604c45310a1e03b087129511a269e
aimok04 <aimok04@mailbox.org>
2026-04-07 16:56:27 +0200

    release: 1.0.0-alpha06

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "app.podiumpodcasts.podium"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1000005
-        versionName = "1.0.0-alpha05"
+        versionCode = 1000006
+        versionName = "1.0.0-alpha06"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
